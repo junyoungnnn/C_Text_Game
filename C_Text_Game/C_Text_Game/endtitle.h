07dfd3fbf557dc000ae5dc5ac779ce252020084e
Jun-Young Noh <119721103+junyoungnnn@users.noreply.github.com>
2023-09-03 18:00:50 +0900

    (2023.09.03) 수정??

---
+++ C_Text_Game/C_Text_Game/endtitle.h
@@ -0,0 +1,55 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+#include <windows.h>
+#include <time.h>
+
+void End_Title()
+{
+	printf("┌─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┐\n");
+	printf("│      \t\t\t\t\t\t\t\t\t\t\t\t\t\t      │\n");
+	printf("│      \t\t\t\t\t\t\t\t\t\t\t\t\t\t      │\n");
+	printf("│      \t\t\t\t\t\t\t\t\t\t\t\t\t\t      │\n");
+	printf("│      \t\t\t\t\t\t\t\t\t\t\t\t\t\t      │\n");
+	printf("│      \t\t\t\t\t\t      SHOOT GAME!\t\t\t\t\t\t      │\n");
+	printf("│      \t\t\t\t\t\t\t\t\t\t\t\t\t\t      │\n");
+	printf("│      \t\t\t\t\t\t\t\t\t\t\t\t\t\t      │\n");
+	printf("│      \t\t\t\t\t\t\t\t\t\t\t\t\t\t      │\n");
+	printf("│      \t\t\t\t\t\t\t\t\t\t\t\t\t\t      │\n");
+	printf("│      \t\t\t\t\t\t\t\t\t\t\t\t\t\t      │\n");
+	printf("│      \t\t\t\t\t\t\t\t\t\t\t\t\t\t      │\n");
+	printf("│      \t\t\t\t\t\t\t\t\t\t\t\t\t\t      │\n");
+	printf("│      \t\t\t\t\t      시작하려면 SPACE를 누르세요\t\t\t\t\t      │\n");
+	printf("│      \t\t\t\t\t\t\t\t\t\t\t\t\t\t      │\n");
+	printf("│      \t\t\t\t\t\t\t\t\t\t\t\t\t\t      │\n");
+	printf("│      \t\t\t\t\t\t\t\t\t\t\t\t\t\t      │\n");
+	printf("│      \t\t\t\t\t\t\t\t\t\t\t\t\t\t      │\n");
+	printf("│      \t\t\t\t\t\t\t\t\t\t\t\t\t\t      │\n");
+	printf("│      \t\t\t\t\t\t\t\t\t\t\t\t\t\t      │\n");
+	printf("│      \t\t\t\t\t\t\t\t\t\t\t\t\t\t      │\n");
+	printf("│      \t\t\t\t\t\t\t\t\t\t\t\t\t\t      │\n");
+	printf("│      \t\t\t\t\t\t\t\t\t\t\t\t\t\t      │\n");
+	printf("│      \t\t\t\t\t\t\t\t\t\t\t\t\t\t      │\n");
+	printf("│      \t\t\t\t\t\t\t\t\t\t\t\t\t\t      │\n");
+	printf("│      \t\t\t\t\t\t\t\t\t\t\t\t\t\t      │\n");
+	printf("│      \t\t\t\t\t\t\t\t\t\t\t\t    #제작자 : 노준영  │\n");
+	printf("│      \t\t\t\t\t\t\t\t\t\t\t\t\t\t      │\n");
+	printf("└─────────────────────────────────────────────────────────────────────────────────────────────────────────────────────┘\n");
+
+	char key = 0;
+	while (1)
+	{
+		if (_kbhit())
+		{
+			key = _getch();
+			switch (key)
+			{
+			case 32:
+				break;
+			}
+			//break;
+		}
+	}
+}
