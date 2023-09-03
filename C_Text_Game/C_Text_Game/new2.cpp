423d9d5db9a9adf96f6eff3501f66928e13d5a4c
Jun-Young Noh <119721103+junyoungnnn@users.noreply.github.com>
2023-09-03 13:04:31 +0900

    (2023.09.03) 완성??

--- C_Text_Game/C_Text_Game/new2.cpp
+++ C_Text_Game/C_Text_Game/new2.cpp
@@ -1,4 +1,3 @@
-// 총알 한개로 변경 총알 구조체 배열-> 일반
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
@@ -18,8 +17,6 @@
 
 #define Player_x_limit 60
 #define Player_y_limit 7
-
-#define BULLET_SIZE 10
 
 bool bullet_check = false;
 
@@ -49,7 +46,6 @@ struct Player_wings
 	const char* backwing;
 };
 
-
 struct Enemy
 {
 	int x, y;
@@ -73,25 +69,6 @@ struct Item
 void gotoXY(int, int);
 void KeyBoard(Player*, Bullet*);
 
-// 맵 생성
-/*
-void CreateMap(struct Interface* player_interface)
-{
-	printf("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■\n");
-	printf("■\t\t\t\t\t\t\t\t\t\t\t\t\t\t■\n");
-	printf("■\t\t\t\t\t\t\t\t\t\t\t\t\t\t■\n");
-	printf("■\t\ttime : %.1f\t\thp : %d\t\t\tscore : %d\t\t\t\t\t■\n", player_interface->time, player_interface->hp, player_interface->score);
-	printf("■\t\t\t\t\t\t\t\t\t\t\t\t\t\t■\n");
-	printf("■\t\t\t\t\t\t\t\t\t\t\t\t\t\t■\n");
-	printf("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■\n");
-	for (int i = 8; i < HEIGHT - 1; i++)
-	{
-		printf("\n");
-	}
-	printf("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■\n");
-}
-*/
-
 // 적 y축 렌덤 셍성
 int seed;
 int Random()
@@ -104,11 +81,8 @@ int Random()
 int bullet_Y;
 void Shoot(Player* player, Bullet* bullet, bool* bullet_check)
 {
-	//bullet[bullet_index].x = player->x;
-	
 	*bullet_check = true;
 	bullet_Y = player->y;
-
 }
 
 // 총알 이동 함수
@@ -117,16 +91,12 @@ void Bullet_Move(Player* player, Bullet* bullet, bool* bullet_check)
 
 	if (*bullet_check == true)
 	{
-			gotoXY(bullet->x+=2, bullet_Y);
-
-			//printf("%s", bullet->shape);
-
-		}
-		if (bullet->x == WIDTH)
-		{
-			*bullet_check = false;
-			
-		}
+		gotoXY(bullet->x += 6, bullet_Y);
+	}
+	if (bullet->x == WIDTH || bullet->x == WIDTH + 1 || bullet->x == WIDTH + 2 || bullet->x == WIDTH + 3 || bullet->x == WIDTH + 4 || bullet->x == WIDTH + 5)
+	{
+		*bullet_check = false;
+	}
 }
 
 // 총알 위치를 플레이어에게 맞추는 함수
@@ -195,12 +165,10 @@ void Collision(struct Interface* player_interface, struct Player* player, struct
 	}
 }
 
-
 // 총알에 맞은 적 파괴
 void Distory(Player* player, Bullet* bullet, Enemy* enemy, bool* enemy_flag, Interface* player_interface, bool* bullet_check)
 {
-
-	if (*bullet_check != false && (bullet->x == enemy->x || bullet->x == enemy->x+1 || bullet->x == enemy->x+2) && bullet->y == enemy->y)
+	if (*bullet_check != false && (bullet->x == enemy->x || bullet->x == enemy->x+1 || bullet->x == enemy->x+2 || bullet->x == enemy->x + 3 || bullet->x == enemy->x + 4 || bullet->x == enemy->x + 5 || bullet->x == enemy->x + 6) && bullet->y == enemy->y)
 	{
 		player_interface->score += 100;
 		*enemy_flag = false;
@@ -223,6 +191,7 @@ void Recovery(Player* player, Item* item, Interface * player_interface, bool* it
 	{
 		*item_flag = true;
 	}
+	// 끝에 도달하면 플래그 false
 	if (*item_flag == true)
 	{
 		gotoXY(item->x--, item->y);
@@ -233,11 +202,13 @@ void Recovery(Player* player, Item* item, Interface * player_interface, bool* it
 			*item_flag = false;
 		}
 	}
+	// 아이템 위치 초기화
 	if (*item_flag == false)
 	{
 		item->x = WIDTH;
 		item->y = Random();
 	}
+	// 회복
 	if (player->x == item->x && player->y == item->y)
 	{
 		*item_flag = false;
@@ -249,51 +220,45 @@ void Recovery(Player* player, Item* item, Interface * player_interface, bool* it
 	}
 }
 
-
-
 int main()
 {
 
-	//system("mode con cols=10 lines=10");
-
 	Init();
 	srand(time(NULL));
+
 	bool enemy_flag[5] = { false, false, false, false, false };
 	bool item_flag = false;
 
 	Player player = { 6,18," →" };
 	Player_wings player_wings = { player.x, player.y - 1, player.x, player.y + 1, player.x - 1, player.y, "▲", "▼", "▶" };
 	Interface player_interface = { 0, 0, 0, "HP: ", "♥♥♥♥♥", "Score: ", 0};
-	Enemy enemy[5] = { {WIDTH, 0, "  "},{WIDTH, 0, "★  "},{WIDTH, 0, "★  "},{WIDTH, 0, "★  "},{WIDTH, 0, "★  "} };
-	//Bullet bullet[10] = { {player.x, player.y, "→"},{player.x, player.y, "→"},{player.x, player.y, "→"},{player.x, player.y, "→"},{player.x, player.y, "→"},{player.x, player.y, "→"},{player.x, player.y, "→"},{player.x, player.y, "→"},{player.x, player.y, "→"},{player.x, player.y, "→"} };
+	Enemy enemy[5] = { {WIDTH, 0, "  "},{WIDTH, 0, "★          "},{WIDTH, 0, "★          "},{WIDTH, 0, "★          "},{WIDTH, 0, "★          "} };
 	Bullet bullet = { player.x, player.y, "*→" };
 	Item item = { WIDTH, 0, "♥" };
 
 	while (1)
 	{
+		// 플레이어 이동
+		KeyBoard(&player, &bullet);
+		gotoXY(player.x, player.y);
 
-
-		//CreateMap(&player_interface);
-		//player_interface.time += 0.1;
+		// 회복 아이템
 		Recovery(&player, &item, &player_interface, &item_flag);
 		if (item_flag == true)
 		{
 			ShowBuffer(item.x, item.y, item.shape);
 		}
 		
-
-		KeyBoard(&player, &bullet);
-		gotoXY(player.x, player.y);
-		//printf("%s", player.shape);
-
+		// 총알 이동
 		Bullet_Move(&player, &bullet, &bullet_check);
 		Bullet_Position(&player, &bullet, &bullet_check);
 
-
+		// 적생성을 위한 플래그
 		Start_flag(&enemy_flag[0]);
 		Create_enemy(&enemy[0], &enemy_flag[0]);
 		Enemy_Reset(&enemy[0], &enemy_flag[0]);
 
+		// 적 생성
 		if (enemy[0].x == 106) 
 		{
 			Start_flag(&enemy_flag[1]);
@@ -326,18 +291,19 @@ int main()
 		Distory(&player, &bullet, &enemy[4], &enemy_flag[4], &player_interface, &bullet_check);
 		Enemy_Reset(&enemy[4], &enemy_flag[4]);
 
+		// 충돌 판정
 		Collision(&player_interface, &player, enemy);
 
-		
-
+		// 플레이어 
 		ShowBuffer(player.x, player.y, player.shape);
 
+		// 날개 
 		Wings_Position(&player, &player_wings);
 		ShowBuffer(player_wings.leftwing_x, player_wings.leftwing_y, player_wings.leftwing);
 		ShowBuffer(player_wings.rightwing_x, player_wings.rightwing_y, player_wings.rightwing);
 		ShowBuffer(player_wings.backwing_x, player_wings.backwing_y, player_wings.backwing);
 	
-		//ShowBuffer(enemy[0].x, enemy[0].y, enemy[0].shape);
+		// 적
 		if (enemy_flag[1] == true)
 		{
 			ShowBuffer(enemy[1].x, enemy[1].y, enemy[1].shape);
@@ -354,9 +320,10 @@ int main()
 		{
 			ShowBuffer(enemy[4].x, enemy[4].y, enemy[4].shape);
 		}
-		
+		// 총알 
 		ShowBuffer(bullet.x, bullet.y, bullet.shape);
 
+		// 인터페이스
 		char score[10];
 		sprintf(score, "%d", player_interface.score);
 		ShowBuffer(player_interface.x + 30, player_interface.y, player_interface.hp_text);
@@ -364,15 +331,15 @@ int main()
 		ShowBuffer(player_interface.x + 60, player_interface.y, player_interface.score_text);
 		ShowBuffer(player_interface.x + 68, player_interface.y, score);
 
-		Sleep(100);
+		Sleep(25);
 		system("cls");
 
-		// 2. 버퍼교체
+		// 버퍼교체
 		Flipping();
 
 		Clear();
 	}
-	// 4. 버퍼를 해제합니다.
+	// 버퍼를 해제
 	ReleaseScreen();
 	return 0;
 }
